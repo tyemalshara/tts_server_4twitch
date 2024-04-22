c764501ee538a64193450850f0b0af8d6ec69c8f
Taim <84383288+tyemalshara@users.noreply.github.com>
2024-04-22 18:46:28 +0200

    update app.py

--- app.py
+++ app.py
@@ -1,19 +1,8 @@
 from flask import Flask, Response,render_template
-import pyaudio
 import socket
 
 app = Flask(__name__)
 
-
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-CHUNK = 1024
-RECORD_SECONDS = 5
-
- 
-audio1 = pyaudio.PyAudio()
- 
 def TwitchConnect(channel_name, myusername, my_oauth_token):
     # Connect to the Twitch chat server
     sock = socket.create_connection(('irc.chat.twitch.tv', 6667))
@@ -36,24 +25,6 @@ def add_silence(audio_data, duration_seconds, sample_rate):
     print(silence)
     return audio_data + silence
 
-
-def genHeader(sampleRate, bitsPerSample, channels):
-    datasize = 2000*10**6
-    o = bytes("RIFF",'ascii')                                               # (4byte) Marks file as RIFF
-    o += (datasize + 36).to_bytes(4,'little')                               # (4byte) File size in bytes excluding this and RIFF marker
-    o += bytes("WAVE",'ascii')                                              # (4byte) File type
-    o += bytes("fmt ",'ascii')                                              # (4byte) Format Chunk Marker
-    o += (16).to_bytes(4,'little')                                          # (4byte) Length of above format data
-    o += (1).to_bytes(2,'little')                                           # (2byte) Format type (1 - PCM)
-    o += (channels).to_bytes(2,'little')                                    # (2byte)
-    o += (sampleRate).to_bytes(4,'little')                                  # (4byte)
-    o += (sampleRate * channels * bitsPerSample // 8).to_bytes(4,'little')  # (4byte)
-    o += (channels * bitsPerSample // 8).to_bytes(2,'little')               # (2byte)
-    o += (bitsPerSample).to_bytes(2,'little')                               # (2byte)
-    o += bytes("data",'ascii')                                              # (4byte) Data Chunk Marker
-    o += (datasize).to_bytes(4,'little')                                    # (4byte) Data size in bytes
-    return o
-
 @app.route('/audio')
 def audio():
     # start Recording
@@ -64,19 +35,40 @@ def audio():
         #     audio = f.read()
         # return audio
         first_run = True
+        sock = TwitchConnect(channel_name='sharatye', myusername='sharatye', my_oauth_token='4csmm85vwecklxgxsts9npswxqjf4d')  
         while True:
-            if first_run:
-                with open("POC_Deepvoice.mp3", "rb") as f:
-                    audio = f.read()
-                framerate = 48000 # or whatever you prefer
-                silenceduration = 1 # or how many seconds you want to produce
-                bytesperframe = 2 # 2 -> 16 bit
-                silence = bytes(
-                        0 for i in range(int(silenceduration * framerate) * bytesperframe))
-                audio = audio + silence
-                first_run = False
+            # if first_run:
+            data = sock.recv(1024).decode()
+            if data.startswith('PING'):
+                sock.send('PONG\r\n'.encode())
+            elif 'PRIVMSG' in data:
+                username = data.split('!')[0][1:]
+                message = str(data.split(':')[2][:-1])
+                if '!tts' in message:
+                    try:
+                        tts_message = message.split('!tts ')[1]
+                        if tts_message == '':
+                            # TODO:send message to the user that the message is empty
+                            continue
+                        elif len(tts_message) > 0:
+                            print(f'{username}: {message}')
+                            with open("POC_Deepvoice.mp3", "rb") as f:
+                                audio = f.read()
+                            framerate = 48000 # or whatever you prefer
+                            silenceduration = 1 # or how many seconds you want to produce
+                            bytesperframe = 2 # 2 -> 16 bit
+                            silence = bytes(
+                                    0 for i in range(int(silenceduration * framerate) * bytesperframe))
+                            audio = audio + silence
+                            first_run = False
+                            yield(audio)
+                    except Exception as e:
+                        # TODO: send message to the user that the message is invalid
+                        print('message is invalid')
+            elif 'PRIVMSG' not in data:
+                audio = b''
                 yield(audio)
-            else:
+            # else:
                # yield empty bytes to keep the connection alive for streaming audio 
                 audio = b''
                 yield(audio)
